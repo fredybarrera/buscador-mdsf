c86275dca5ffd570b04295896c7d92bb6b72232a
Fredys Barrera <fredy.barrera.a@gmail.com>
2020-11-20 12:54:52 -0300

    Commit inicial

---
+++ widgets/Report/setting/nls/strings.js
@@ -0,0 +1,10 @@
+define({
+	root: ({
+		configText: "Set config text:",
+		_widgetLabel: "Demo",
+    	generateReport: "Generate Report",
+    	name: "aaaaa",
+    	sssss: "sadasdasd"
+  }),
+  "es": 1
+});
